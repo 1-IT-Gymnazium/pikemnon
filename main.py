eb5298ca3a5e6e79f2ad35f4b205c5e83e679623
Zdeněk Sojka <code-stuff.m52iv@aleeas.com>
2023-12-08 01:38:03 +0100

    Created Base

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyglet
+
+# Window dimensions
+window_width = 800
+window_height = 600
+
+# Create a window
+window = pyglet.window.Window(window_width, window_height, "Pikemnon")
+
+if __name__ == '__main__':
+    pyglet.app.run()
