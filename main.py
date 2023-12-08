5197d3096b9d9d339651fe2fc25840c31ddb2a97
Zdeněk Sojka <code-stuff.m52iv@aleeas.com>
2023-12-08 01:42:03 +0100

    Drawing player

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pyglet
+from pyglet.gl import *
+from pyglet.window import key
 
 # Window dimensions
 window_width = 800
@@ -6,6 +8,23 @@ window_height = 600
 
 # Create a window
 window = pyglet.window.Window(window_width, window_height, "Pikemnon")
+
+image = pyglet.resource.image('player.png')
+
+# Ensure the image scales pixel-perfectly
+texture = image.get_texture()
+glTexParameteri(texture.target, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+glTexParameteri(texture.target, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+
+# Create a sprite and scale it up
+sprite = pyglet.sprite.Sprite(img=image, x=window.width//2, y=window.height//2)
+sprite.scale = 4
+
+# Draw the sprite
+@window.event
+def on_draw():
+    window.clear()
+    sprite.draw()
 
 if __name__ == '__main__':
     pyglet.app.run()
