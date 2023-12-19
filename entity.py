4c7d418f7d3b25c27c970bc2df399db1eb972627
Zdeněk Sojka <code-stuff.m52iv@aleeas.com>
2023-12-19 11:30:49 +0100

    Added player movement normalization

--- entity.py
+++ entity.py
@@ -2,7 +2,7 @@ import pyglet
 from pyglet.gl import *
 
 class Entity:
-    def __init__(self, image_file, x, y, scale=4):
+    def __init__(self, image_file, x, y, health=100):
         self.image = pyglet.resource.image(image_file)
 
         texture = self.image.get_texture()
@@ -10,7 +10,9 @@ class Entity:
         glTexParameteri(texture.target, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
 
         self.sprite = pyglet.sprite.Sprite(img=self.image, x=x, y=y)
-        self.sprite.scale = scale
+        self.sprite.scale = 4
+
+        self.health = health
 
     def draw(self):
         self.sprite.draw()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyglet
 from pyglet.gl import *
-from entity import Entity
+from pyglet.window import key
+from player import Player
 
 # Window dimensions
 window_width = 800
@@ -9,9 +10,43 @@ window_height = 600
 # Create a window
 window = pyglet.window.Window(window_width, window_height, "Pikemnon")
 
-player = Entity('assets/player.png', window.width//2, window.height//2)
+player = Player('assets/player.png', window.width//2, window.height//2)
 
-# Draw the sprite
+key_state = {
+    'up': False,
+    'down': False,
+    'left': False,
+    'right': False
+}
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key.W:
+        key_state['up'] = True
+    elif symbol == key.S:
+        key_state['down'] = True
+    elif symbol == key.A:
+        key_state['left'] = True
+    elif symbol == key.D:
+        key_state['right'] = True
+
+@window.event
+def on_key_release(symbol, modifiers):
+    if symbol == key.W:
+        key_state['up'] = False
+    elif symbol == key.S:
+        key_state['down'] = False
+    elif symbol == key.A:
+        key_state['left'] = False
+    elif symbol == key.D:
+        key_state['right'] = False
+
+def update(dt):
+    player.update(dt, key_state)
+
+pyglet.clock.schedule_interval(update, 1/60.0)
+
+
 @window.event
 def on_draw():
     window.clear()
